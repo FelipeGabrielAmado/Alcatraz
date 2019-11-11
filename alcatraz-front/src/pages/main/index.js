8e433eddf89758c237c1ce3be16561ebc8531bf5
Felipe Gabriel Amado <felipe@dblinks.com.br>
2019-11-11 15:33:53 -0200

    Added owl carousel to main page

--- alcatraz-front/src/pages/main/index.js
+++ alcatraz-front/src/pages/main/index.js
@@ -1,47 +1,74 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import OwlCarousel from 'react-owl-carousel2';
+
 import './styles.css'
 import Banner from '../../components/Banner'
 
+
 export default class Main extends Component {
     state = {
-        movies : []
+        movies: []
 
 
     };
 
+
     async componentDidMount() {
         const response = await api.get(`/movies`);
         console.log(response)
-        this.setState({ movies: response.data});
+        this.setState({ movies: response.data });
     }
 
 
 
     render() {
         const { movies } = this.state;
-        
-        if(!movies) return null;
 
-        console.log(typeof(movies))
+        const options = {
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        };
 
-        return(
+        if (!movies) return null;
+
+        return (
             <div>
-                <Banner/>
+                <Banner />
                 <div className="category-name"><p>New Movies</p></div>
+                <OwlCarousel ref="car" options={options}>
                     <div className="movie-list">
                         {movies.map(movie => (
+
                             <article key={movie.id}>
                                 <div className="poster-box">
-                                    <img src={movie.poster} alt="Minha Figura"/>
-                                    <div className="movie-title">{movie.name}</div>
+                                    <img src={movie.poster} alt="Minha Figura" />
+                                    <div className="movie-info">
+                                        <div className="movie-title"> {movie.name} </div>
+                                        <div className="movie-genre"> {movie.genre} </div>
+                                        <div className="movie-rating"> <FontAwesomeIcon icon={faStar} /> {movie.rating} </div>
+                                    </div>
                                 </div>
                             </article>
+
                         ))}
-                        
                     </div>
-            </div>
+                </OwlCarousel>
+            </div >
         )
     }
 }
