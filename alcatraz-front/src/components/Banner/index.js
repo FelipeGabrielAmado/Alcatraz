979ba62be87b04932e5e1b8354eb5dfd1161a02b
Felipe Amado <felipegabrielamado@gmail.com>
2020-01-16 13:25:44 -0300

    Changed banner style

--- alcatraz-front/src/components/Banner/index.js
+++ alcatraz-front/src/components/Banner/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './styles.css'
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from 'react-responsive-carousel'
+import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const Banner = () => (
-    <Carousel showThumbs={ false } autoPlay={ true } >
-        <div className="banner">
-            <img src="images/banner-starwars.jpg" />
+
+    <div className="banner">
+        <div className='banner-info'>
+            <div className='banner-movie-title'> Joker </div>
+            <div className='banner-movie-info'> Crime, Drama, Thriller | Duration: 2h 2min </div>
+            <div className='banner-buttons'>
+                <a href='http://localhost:3001/movie/1'> <div className='banner-watch-button'> WHERE TO WATCH </div> </a>
+                <a href='https://www.imdb.com/title/tt7286456/?ref_=nv_sr_srsg_0'>
+                    <div className='rating-button'>
+                        <div className='banner-rating'>
+                            Rating <div className='rating-source'>based on IMDB</div>
+                        </div>
+                        <div className='banner-stars'><FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStarHalf} /></div>
+                    </div>
+                </a>
+            </div>
         </div>
-    </Carousel>
+        <img src="images/banner-joker.jpg" />
+    </div >
+
 );
 
 export default Banner;
