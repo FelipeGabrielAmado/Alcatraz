12c8eadafa74cb1987726269cb93525d1ebfcb6a
Felipe Amado <felipegabrielamado@gmail.com>
2020-01-24 15:12:33 -0300

    Styled Input

--- alcatraz-admin/src/components/SideMenu/index.js
+++ alcatraz-admin/src/components/SideMenu/index.js
@@ -4,14 +4,18 @@ import './styles.css'
 export default class SideMenu extends Component {
 
     render() {
+
+        console.log(window.location.pathname);
+
         return (
             <div class="sidenav">
                 <a href='http://localhost:3001/'> <img src="/images/logo.png" alt="Logo Alcatraz" /> </a>
                 <div className='items-menu'>
                         <div className='category-title'>Main</div>
-                        <a href="/">Dashboard</a>
-                        <a href="/movies">Movies</a>
-                        <a href="/categories">Categories</a>
+
+                        <div className={(window.location.pathname === '/') ? "active-item" : ""}>   <a href="/">Dashboard</a>               </div>
+                        <div className={(window.location.pathname === '/movies') ? "active-item" : ""}>   <a href="/movies">Movies</a>            </div>
+                        <div className={(window.location.pathname === '/categories') ? "active-item" : ""}>   <a href="/categories">Categories</a>    </div>
                 </div>
             </div>
         );
